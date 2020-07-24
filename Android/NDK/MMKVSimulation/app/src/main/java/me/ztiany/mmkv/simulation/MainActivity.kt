c2cab956d75f3c23727dc8c560cd288a1cd381d1
Ztiany <Ztiany3@gmail.com>
2020-07-24 18:13:27 +0800

    add mmkv

--- Android/NDK/MMKVSimulation/app/src/main/java/me/ztiany/mmkv/simulation/MainActivity.kt
+++ Android/NDK/MMKVSimulation/app/src/main/java/me/ztiany/mmkv/simulation/MainActivity.kt
@@ -2,29 +2,49 @@ package me.ztiany.mmkv.simulation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import com.yanzhenjie.permission.AndPermission
+import com.yanzhenjie.permission.runtime.Permission
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 class MainActivity : AppCompatActivity() {
 
+    private val nativeBridge by lazy { NativeBridge() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+        askPermission()
 
-        // Example of a call to a native method
-        sample_text.text = stringFromJNI()
-    }
+        btnInit.setOnClickListener {
+            val file = File(getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS), "mmap/text.txt")
+            file.createNew()
+            nativeBridge.mmapInit(file.absolutePath)
+        }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    external fun stringFromJNI(): String
+        btnMMapWrite.setOnClickListener {
+            nativeBridge.mmapWrite()
+        }
 
-    companion object {
-
-        // Used to load the 'native-lib' library on application startup.
-        init {
-            System.loadLibrary("native-lib")
+        btnMMapRead.setOnClickListener {
+            nativeBridge.mmapRead()
         }
     }
+
+    private fun askPermission() {
+        AndPermission.with(this)
+            .runtime()
+            .permission(Permission.WRITE_EXTERNAL_STORAGE)
+            .onDenied {
+                supportFinishAfterTransition()
+            }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        nativeBridge.destroy()
+    }
+
 }
