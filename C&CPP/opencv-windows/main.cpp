fd346bb57fdc0971cb2a6e6b23a4882056a4c1bc
ztiany <ztiany3@gmail.com>
2020-08-22 00:02:36 +0800

    update

--- C&CPP/opencv-windows/main.cpp
+++ C&CPP/opencv-windows/main.cpp
@@ -6,11 +6,11 @@ using namespace cv;
 using namespace std;
 
 int main() {
-    string path_id1("img/id1.jpg");
-    //cv_base::erodeImage();
+    string path_id1("img/scenery1.jpg");
+    cv_base::erodeImage(path_id1);
     //cv_base::blurImage();
     //cv_base::cannyImage(path_id1);
-    cv_base::findIdentityCardNumber(path_id1);
+    //cv_base::findIdentityCardNumber(path_id1);
     //cv_base::findIdentityCardNumber2(path_id1);
     return EXIT_SUCCESS;
 }
