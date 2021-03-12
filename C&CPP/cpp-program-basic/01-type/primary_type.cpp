c656deefd99fa83e576166d30dd04b325d77ce88
ztiany <ztiany3@gmail.com>
2021-03-13 00:58:39 +0800

    update

--- C&CPP/cpp-program-basic/01-type/primary_type.cpp
+++ C&CPP/cpp-program-basic/01-type/primary_type.cpp
@@ -10,10 +10,10 @@
 #include <iostream>//输入输出
 #include <climits>//包含各种整型类型的系统限制
 #include "cfloat"//包含各种浮点类型的系统限制
-
+#include <cstdio>
+#include <cstdlib>
 
 int main() {
-
     /*-----------------short类型-----------------*/
     short sa = 22;
     unsigned short usa = 32u;//u作为后缀表示无符号，用于指定字面值类型
@@ -49,13 +49,13 @@ int main() {
     //字符：wchar_t是一种整数类型，它有足够的空间，可以表示系统的最大扩展字符集
     //wchar_t底层的类型取决于编译器实现，在一种系统中可能是unsigned short，在另一种系统中又可能是int。
     //cin和cout将输入和输出看作是字符流，因此不适合用来处理whcar_t类型。
-    wchar_t w_char = '我';
+    wchar_t w_char = L'我';
     wchar_t l_w_char = L'p';//使用L来指示宽字符常量和宽字符串
+    wprintf (L"Characters: %lc %lc \n", L'a', w_char);
 
     //C11：char16_t和char32_t是内置类型，用于处理unicode字符，它们都是无符号的，但是底层类型可能跟随系统
     char16_t char_16 = u'q';
     char32_t char_32 = U'我';
-
 
     /*-----------------布尔类型-----------------*/
     bool is_real = true;
